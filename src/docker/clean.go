d8615f40c82a49e11d8fc47ea4556547184a2c83
Vineel Sai <mail@vineelsai.com>
2023-02-12 18:17:39 +0530

    fix: autoremove (#20)
    * fix: autoremove
    
    * fix: manual clean
    
    * fix: import

--- src/docker/clean.go
+++ src/docker/clean.go
@@ -5,15 +5,16 @@ import (
 	"os"
 	"strings"
 
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
 func clean(cli *client.Client, response container.CreateResponse, ctx context.Context, fileName string) {
 	// // remove the container
-	// if err := cli.ContainerRemove(ctx, response.ID, types.ContainerRemoveOptions{}); err != nil {
-	// 	panic(err)
-	// }
+	if err := cli.ContainerRemove(ctx, response.ID, types.ContainerRemoveOptions{}); err != nil {
+		panic(err)
+	}
 
 	// remove the file
 	if err := os.Remove(fileName); err != nil {
